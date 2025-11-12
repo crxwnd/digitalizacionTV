07d47b793ac3d09c5ab51666d747476677e3e002
Crownd <zeroeduardo166@gmail.com>
2025-11-12 13:41:12 -0500

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 // frontend/src/App.tsx
+//Cambio prueba
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Layout from './components/Layout';
